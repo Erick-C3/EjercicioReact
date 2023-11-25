13eb7487ec00006689571020ed943a934d648a64
Erick <erick.nahuelpaiva.001@gmail.com>
2023-11-25 18:53:44 -0300

    agregar componente carrito e iconos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useContext, useEffect, useState } from 'react'
+import {useEffect, useId, useState } from 'react'
 import './App.css'
 import Tarjeta from './componentes/Tarjeta.jsx'
 import { ProductoProvider } from './contexto/producto.jsx';
+import CarritoMenu from './componentes/CarritoMenu.jsx';
+import IconoCarrito from './iconos/IconoCarrito.jsx';
 
 /* const DEF_TARJETA = <ProductoProvider>
   <Tarjeta
@@ -14,22 +16,23 @@ import { ProductoProvider } from './contexto/producto.jsx';
 
 function App() {
   const [productos, setProductos] = useState(/* DEF_TARJETA */ "DEF PROD");
+  const idApp = useId();
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("Effect");
 
-    async function pedirProductos(){
+    async function pedirProductos() {
       const res = await fetch("http://localhost:4000/productos");
       let productos = "def producto";
       const tempComponentTarjetas = []
-      if(res.ok){
+      if (res.ok) {
         productos = await res.json();
         console.log(productos);
         productos.forEach(producto => {
           tempComponentTarjetas.push(
-            <ProductoProvider key={"prod-provider-"+producto.id} cantidadInicial={producto.cantidad}>
+            <ProductoProvider key={"prod-provider-" + producto.id} cantidadInicial={producto.cantidad}>
               <Tarjeta
-                key={"tarjeta-"+producto.id}
+                key={"tarjeta-" + producto.id}
                 nombre={producto.nombre}
                 precio={producto.precio}
                 imagen={producto.imagen}
@@ -43,16 +46,23 @@ function App() {
 
     pedirProductos()
 
-    return ()=>{
+    return () => {
       console.log("Limpiando efecto");
     }
-  },[]);
+  }, []);
 
 
   return (
-    <div id='productos'>
-      {productos}
-    </div>
+    <>
+      <CarritoMenu
+        key={idApp+"carrito-menu"}
+        placement="end"
+        name={<IconoCarrito.Menu/>}
+      />
+      <div id='productos'>
+        {productos}
+      </div>
+    </>
   )
 }
 
